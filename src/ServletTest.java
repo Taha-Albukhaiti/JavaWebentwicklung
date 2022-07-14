fff9e01435b0ab4c7c048f717ff9636ee29fada0
Taha-Albukhaiti <tahaalbukhaiti@gmail.com>
2022-07-14 14:38:07 +0200

    $

--- src/ServletTest.java
+++ src/ServletTest.java
@@ -1,16 +1,47 @@
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import java.io.*;
 
 
 public class ServletTest extends HttpServlet {
+
+    private static final String htm = "index.html";
+    private static final String css = "design.html";
+    private static final String js = "script.html";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String fooValue = req.getParameter("foo");
         ServletOutputStream out = resp.getOutputStream();
-        out.println("<h1>Hello World!</h1>");
-        out.println("<span>Der Wert des Parameters foo war " + fooValue + "</span>");
+        String text = dateiLesen(htm);
+
+        out.println(text);
+        out.println();
+
+
+    }
+
+    public static String dateiLesen(String datei_name) {
+        BufferedReader bf = null;
+        String text = null;
+        try {
+            bf  = new BufferedReader(new FileReader(datei_name));
+            while (bf.ready()) {
+                text = bf.readLine();
+            }
+            return text;
+
+        } catch (IOException e) {
+            e.getStackTrace();
+        } finally {
+            try {
+                bf.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return text;
     }
 }
